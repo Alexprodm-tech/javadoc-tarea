ecdf44a5b75c76287816ce271839d8fdb0164ab5
P-Droid <padiazmasis@gmail.com>
2025-11-26 20:19:51 -0600

    Add files via upload

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"<Unnamed>","l":"Alquileres"},{"p":"<Unnamed>","l":"Carros"},{"p":"<Unnamed>","l":"Clientes"},{"p":"<Unnamed>","l":"PruebaDeTodo"}];updateSearchResults();
